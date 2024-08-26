3e8db61895e13858bb303f6a8d71af91888a5c8e
Bohdan Kret <kretbohdan2002@gmail.com>
2024-08-26 18:33:22 +0300

    Update task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,18 +1,27 @@
-const nameImput = document.querySelector("#name-input");
+const nameInput = document.querySelector("#name-input");
 const nameOutput = document.querySelector("#name-output");
 
-const onImput = () => {
+const onInput = () => {
+  nameOutput.textContent = nameInput.value;
+  nameInput.value = nameInput.value.trim();
+
+  if (nameInput.value === "" || nameInput.value === " ") {
+    nameOutput.textContent = "Anonymous";
+  }
+
+  // Other diffrent methods(solutions)
+
   // if (!nameImput.value.replace(/\s/g, "")) {
   //   nameOutput.textContent = "Anonymous";
   // } else {
   //   nameOutput.textContent = nameImput.value;
   // }
 
-  if (!nameImput.value.split(" ").join("")) {
-    nameOutput.textContent = "Anonymous";
-  } else {
-    nameOutput.textContent = nameImput.value;
-  }
+  // if (!nameInput.value.split(" ").join("")) {
+  //   nameOutput.textContent = "Anonymous";
+  // } else {
+  //   nameOutput.textContent = nameInput.value;
+  // }
 };
 
-nameImput.addEventListener("input", onImput);
+nameInput.addEventListener("input", onInput);
